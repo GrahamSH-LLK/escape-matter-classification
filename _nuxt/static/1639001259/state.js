853a722ce75ce88fa41f29aa582e1fa4dd856336
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2021-12-08 22:11:05 +0000

    deploy: 4624dc430933184837bff68c2b319bf3521a4536

--- _nuxt/static/1639001259/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fescape-matter-classification\u002F_nuxt\u002Fstatic\u002F1639001259",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fescape-matter-classification\u002F",assetsPath:"\u002Fescape-matter-classification\u002F_nuxt\u002F",cdnURL:a}}}}(null));
