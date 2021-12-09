175ba44287ac08b354b345e725f9bf875efa501c
GrahamSH <grahamshllk@gmail.com>
2021-12-09 09:46:23 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto%20Flat&display=swap'}],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nova+Flat&display=swap'}],
   },
   tailwindcss: { config: {
     mode: 'jit',
